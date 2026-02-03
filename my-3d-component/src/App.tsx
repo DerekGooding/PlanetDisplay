08f34671ec6cefec627a2b8cd3c7839727b27320
Derek Gooding <djgooding@live.com>
2026-02-03 15:51:55 -0500

    fix image and workflow

--- my-3d-component/src/App.tsx
+++ my-3d-component/src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useLoader } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
+import { TextureLoader } from 'three';
 
 // Component to display the sphere
 function Planet() {
-  // The texture has been removed for debugging. We are just rendering a basic sphere.
+  const texture = useLoader(TextureLoader, '/planet_2d.png');
   return (
     <mesh>
       <sphereGeometry args={[2, 32, 32]} />
-      <meshStandardMaterial color="orange" wireframe={true} />
+      <meshStandardMaterial map={texture} transparent={true} />
     </mesh>
   );
 }
@@ -19,7 +18,7 @@ export default function App() {
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 5] }}>
         <color attach="background" args={['#333']} />
-        <ambientLight intensity={1.0} />
+        <ambientLight intensity={1.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
         <Planet />
         <OrbitControls />
